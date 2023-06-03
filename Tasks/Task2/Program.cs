362ae30107e02dd0ed0533326040906e0513c887
AnyutaAntonovich <antonovich_am@bk.ru>
2023-06-03 13:14:12 +0600

    Add task 2 version 1

---
+++ Tasks/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+// по нечетной позиции числа (если индекс четный, то ++)
+
+int[] GetArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(1,100);
+    }
+    return array;
+}
+
+int GetLength()
+{
+    Console.WriteLine("Input the length for array:");
+    int length = Convert.ToInt32(Console.ReadLine());
+    return length;
+}
+
+int Sum(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i+=2)
+    {
+        sum = sum + array[i];
+    }
+    
+    return sum;
+}
+
+int length = GetLength();
+int[] array = GetArray(length);
+string arrayToStr = String.Join(", ", array);
+int sum = Sum(array);
+Console.WriteLine($"In array [{arrayToStr}], total {array.Length} numbers of which the sum of the numbers in odd positions is {sum} are even");
